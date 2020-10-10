d29d9b1d032ccc0104620fb12dbd0a5682dfe908
MArslan <gery_maz_555@hotmail.com>
2020-10-10 09:49:12 +0500

    some modifications

---
+++ app.js
@@ -0,0 +1,20 @@
+// alert("Hello World");
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c)
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log(c);  // show value in console
+
+// var x = "Muhammad ";
+// var y = "Arslan";
+// var z = x+y;
+// alert(z);
+
+var x = 10;
+var y = x++ + ++x -x + x++ - ++x + x;   // 10  + 12 - 12 + 12 - 14 + 14
+alert(y);
